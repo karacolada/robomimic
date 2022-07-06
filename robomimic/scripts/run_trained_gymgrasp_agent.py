780be345b3c44669d726319cc768b80d8ae2c935
Kara Moraw <k.moraw@uni-bonn.de>
2022-07-06 13:04:19 +0200

    make recordings available in trained agent

--- robomimic/scripts/run_trained_gymgrasp_agent.py
+++ robomimic/scripts/run_trained_gymgrasp_agent.py
@@ -4,52 +4,25 @@ The main script for evaluating a policy in an environment.
 Args:
     agent (str): path to saved checkpoint pth file
 
+    n_rollouts (int): number of rollouts to be run
+
     horizon (int): if provided, override maximum horizon of rollout from the one 
         in the checkpoint
 
     env (str): if provided, override name of env from the one in the checkpoint,
         and use it for rollouts
 
-    render (bool): if flag is provided, use on-screen rendering during rollouts
-
-    video_path (str): if provided, render trajectories to this video file path
-
-    video_skip (int): render frames to a video every @video_skip steps
-
-    camera_names (str or [str]): camera name(s) to use for rendering on-screen or to video
-
-    dataset_path (str): if provided, an hdf5 file will be written at this path with the
-        rollout data
-
-    dataset_obs (bool): if flag is provided, and @dataset_path is provided, include 
-        possible high-dimensional observations in output dataset hdf5 file (by default,
-        observations are excluded and only simulator states are saved).
-
     seed (int): if provided, set seed for rollouts
+
+    record (bool): if flag is provided, record rollouts
 
 Example usage:
 
-    # Evaluate a policy with 50 rollouts of maximum horizon 400 and save the rollouts to a video.
-    # Visualize the agentview and wrist cameras during the rollout.
+    # Evaluate a policy with 50 rollouts of maximum horizon 400 and save the recordings.
     
-    python run_trained_agent.py --agent /path/to/model.pth \
+    python run_trained_gymgrasp_agent.py --agent /path/to/model.pth \
         --n_rollouts 50 --horizon 400 --seed 0 \
-        --video_path /path/to/output.mp4 \
-        --camera_names agentview robot0_eye_in_hand 
-
-    # Write the 50 agent rollouts to a new dataset hdf5.
-
-    python run_trained_agent.py --agent /path/to/model.pth \
-        --n_rollouts 50 --horizon 400 --seed 0 \
-        --dataset_path /path/to/output.hdf5 --dataset_obs 
-
-    # Write the 50 agent rollouts to a new dataset hdf5, but exclude the dataset observations
-    # since they might be high-dimensional (they can be extracted again using the
-    # dataset_states_to_obs.py script).
-
-    python run_trained_agent.py --agent /path/to/model.pth \
-        --n_rollouts 50 --horizon 400 --seed 0 \
-        --dataset_path /path/to/output.hdf5
+        --record
 """
 
 import isaacgym
@@ -70,13 +43,6 @@ import robomimic.utils.tensor_utils as TensorUtils
 from robomimic.utils.train_utils import run_parallel_rollouts
 
 def run_trained_agent(args):
-    # some arg checking
-    write_video = (args.video_path is not None)
-    assert not (args.render and write_video) # either on-screen or video but not both
-    if args.render:
-        # on-screen rendering can only support one camera
-        assert len(args.camera_names) == 1
-
     # relative path to agent
     ckpt_path = args.agent
 
@@ -88,6 +54,20 @@ def run_trained_agent(args):
 
     num_envs = ckpt_dict["env_metadata"]["env_kwargs"]["task"]["env"]["numEnvs"]
     ckpt_dict["env_metadata"]["env_kwargs"]["headless"] = True
+
+    if args.record:
+        camera_0 = dict(type="rgb",
+                        pos=[ 0.0, -0.5, 1.3 ],
+                        lookat=[ 0,  0, 0.8 ],
+                        horizontal_fov=70,
+                        width=900,
+                        height=900,
+                    )
+        cameras = dict(save_recordings=True,
+                       convert_to_pointcloud=False,
+                       convert_to_voxelgrid=False,
+                       camera0=camera_0)
+        ckpt_dict["env_metadata"]["env_kwargs"]["task"]["cameras"] = cameras
 
     # read rollout settings
     assert args.n_rollouts % num_envs == 0
@@ -102,8 +82,8 @@ def run_trained_agent(args):
     env, _ = FileUtils.env_from_checkpoint(
         ckpt_dict=ckpt_dict, 
         env_name=args.env, 
-        render=args.render, 
-        render_offscreen=(args.video_path is not None), 
+        render=False, 
+        render_offscreen=False, 
         verbose=True,
     )
 
@@ -112,11 +92,6 @@ def run_trained_agent(args):
         np.random.seed(args.seed)
         torch.manual_seed(args.seed)
 
-    # maybe create video writer
-    video_writer = None
-    if write_video:
-        video_writer = imageio.get_writer(args.video_path, fps=20)
-
     rollout_stats = []
     for i in range(rollout_num_episodes):
         rollout_info = run_parallel_rollouts(
@@ -124,8 +99,7 @@ def run_trained_agent(args):
             env=env,
             horizon=rollout_horizon,
             render=args.render,
-            video_writer=video_writer, 
-            video_skip=args.video_skip, 
+            video_writer=None,
             )
         rollout_stats.append(rollout_info)
         print(f"Iteration {i} rollout stats:")
@@ -135,9 +109,6 @@ def run_trained_agent(args):
     avg_rollout_stats["Num_Success"] = np.sum(rollout_stats["Success_Rate"])
     print("Average Rollout Stats")
     print(avg_rollout_stats)
-
-    if write_video:
-        video_writer.close()
 
 
 if __name__ == "__main__":
@@ -176,60 +147,18 @@ if __name__ == "__main__":
             it for rollouts",
     )
 
-    # Whether to render rollouts to screen
-    parser.add_argument(
-        "--render",
-        action='store_true',
-        help="on-screen rendering",
-    )
-
-    # Dump a video of the rollouts to the specified path
-    parser.add_argument(
-        "--video_path",
-        type=str,
-        default=None,
-        help="(optional) render rollouts to this video file path",
-    )
-
-    # How often to write video frames during the rollout
-    parser.add_argument(
-        "--video_skip",
-        type=int,
-        default=5,
-        help="render frames to video every n steps",
-    )
-
-    # camera names to render
-    parser.add_argument(
-        "--camera_names",
-        type=str,
-        nargs='+',
-        default=["agentview"],
-        help="(optional) camera name(s) to use for rendering on-screen or to video",
-    )
-
-    # If provided, an hdf5 file will be written with the rollout data
-    parser.add_argument(
-        "--dataset_path",
-        type=str,
-        default=None,
-        help="(optional) if provided, an hdf5 file will be written at this path with the rollout data",
-    )
-
-    # If True and @dataset_path is supplied, will write possibly high-dimensional observations to dataset.
-    parser.add_argument(
-        "--dataset_obs",
-        action='store_true',
-        help="include possibly high-dimensional observations in output dataset hdf5 file (by default,\
-            observations are excluded and only simulator states are saved)",
-    )
-
     # for seeding before starting rollouts
     parser.add_argument(
         "--seed",
         type=int,
         default=None,
         help="(optional) set seed for rollouts",
+    )
+
+    parser.add_argument(
+        "--record",
+        action='store_true',
+        help="record rollouts in simulation",
     )
 
     args = parser.parse_args()
